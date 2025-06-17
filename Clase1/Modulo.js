056c559b2ea9706bc18f57f0fc489f7d5edbb415
Julian Gomez <72952395+Julpygo@users.noreply.github.com>
2025-06-16 21:31:52 -0500

    version final

--- Clase1/Modulo.js
+++ Clase1/Modulo.js
@@ -1,8 +1,22 @@
-let InputNum = 5
-let resultado = InputNum % 5
-if (resultado == 0){
-    console.log("El numero es multiplo de 5")
-}
-else{
-    console.log("No es multiplo de 5")
-}
+const entrada = document.getElementById("entrada")
+const calcular = document.getElementById("btn")
+const salida = document.getElementById("salida")
+let numero = 0
+
+entrada.addEventListener("input",(e)=>{
+    numero = e.target.value
+    console.log(numero)
+})
+
+calcular.addEventListener("click",()=>{
+    let hijo = document.createElement("p")
+    if (numero % 5 == 0){
+        hijo.innerHTML = `<b>${numero}</b> es multiplo de 5`
+    //console.log(`${numero} es multiplo de 5}`)
+    }
+    else{
+        hijo.innerHTML = `${numero} <b>no es multiplo de 5</b>`
+        //console.log(`${numero} no es multiplo de 5`)
+    }
+salida.appendChild(hijo)
+})
